dea9717ec71fe0e809cbf4631dba3273e8ed7ccd
jimsong <sjmdhr@163.com>
2021-03-29 01:04:16 -0700

    add sample_parse.h

---
+++ aiarena-framework/shm_lib_dataop/sample_parse.h
@@ -0,0 +1,24 @@
+#ifndef _SAMPLE_PARSE_H
+#define _SAMPLE_PARSE_H
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <fstream>
+#include <unistd.h>
+#include <exception>
+#include <sstream>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include "proto/gym_rl.pb.h"
+
+namespace sample_parse {
+    class SampleParse {
+     private:
+         gym_rl::OfflineRlInfo p_data;
+     public:
+         bool toSample(char* origin_data, int data_len, char* sample_data, std::string mask, int task_id, std::string task_uuid);
+    };
+}
+#endif
